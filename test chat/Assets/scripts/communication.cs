34c95a5dd4e3fba96c13eac90b989e89ad6dd31d
Omar <ouazzani561@gmail.com>
2020-07-27 14:37:59 +0100

    first

---
+++ test chat/Assets/scripts/communication.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class communication : MonoBehaviour
+{
+
+    
+
+    string message;
+    PhotonView view;
+
+    public void Envoyer()
+    {
+
+        view.RPC("methode", PhotonTargets.All);
+
+    }
+
+
+    [PunRPC]
+    private void methode()
+    {
+        GameObject entree = GameObject.Find("InputField");
+        string message;
+        GameObject texte1 = GameObject.Find("Text");
+        Text texte = (Text)texte1;
+
+        message = entree.text;
+        texte.text = message;
+
+    }
+
+}
---
+++ test chat/Assets/scripts/network.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class network : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject canvaser;
+
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings("chat1");
+    }
+
+    void OnJoinedLobby()
+    {
+        RoomOptions MyRoomOptions = new RoomOptions();
+        MyRoomOptions.MaxPlayers = 20;
+        PhotonNetwork.playerName = "Chater" + Random.Range(1, 500);
+        PhotonNetwork.JoinOrCreateRoom("Chat", MyRoomOptions, TypedLobby.Default);
+
+
+    }
+
+    void OnJoinedRoom()
+    {
+        canvaser.SetActive(true);
+        Debug.Log("hello");
+    }
+
+}
